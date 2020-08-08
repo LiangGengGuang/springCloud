a161097b57d7b73273726f5090ac861ee634c163
LiangGengGuang <604046269@qq.com>
2020-08-09 00:21:06 +0800

    no message

--- service-client8010/src/main/java/com/example/serviceClient/ServiceClientApplication8010.java
+++ service-client8010/src/main/java/com/example/serviceClient/ServiceClientApplication8010.java
@@ -3,12 +3,12 @@ package com.example.serviceClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class ServiceClientApplication8010 {
